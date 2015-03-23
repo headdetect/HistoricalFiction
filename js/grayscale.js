795a93bdb5e51a6659bb1a43dc2e49a095472a20
Brayden Lopez <blopez@instructure.com>
2015-03-23 15:49:00 -0600

    Fixed various issues.

--- js/grayscale.js
+++ js/grayscale.js
@@ -37,6 +37,7 @@ $("button.mute").click(function() {
 
 var toggleMute = function(muted) {
     var dom = $("#video").get(0);
+    if(!dom) return;
     dom.muted = (typeof muted == "undefined") ? !dom.muted : !!muted;
 
     var icon = $(".video-controls .mute i");
@@ -60,12 +61,38 @@ var characters = {
     "ACAP": "acap"
 };
 
-var titles = $("h4.timeline-title");
-titles.each(function() {
+var characterLinks = {
+    "CMD": "http://en.wikipedia.org/wiki/Neil_Armstrong",
+    "PIL": "http://en.wikipedia.org/wiki/Michael_Collins_(astronaut)",
+    "LMP": "http://en.wikipedia.org/wiki/Buzz_Aldrin",
+    "HGC": "http://en.wikipedia.org/wiki/Bruce_McCandless_II",
+    "A11": "http://en.wikipedia.org/wiki/Apollo_11",
+    "CAPCOM": "http://en.wikipedia.org/wiki/Bruce_McCandless_II",
+    "MARINE": "http://en.wikipedia.org/wiki/United_States_Marine_Corps#United_States_Navy",
+    "POTUS": "http://en.wikipedia.org/wiki/Richard_Nixon",
+    "DCTR": "http://en.wikipedia.org/wiki/Gene_Kranz",
+    "ACAP": "http://en.wikipedia.org/wiki/Lyndon_B._Johnson_Space_Center"
+};
+
+$("h4.timeline-title").each(function() {
     var title = $(this);
     var image = characters[title.text()];
     if (image) {
         var imageElement = $("<img src='/img/chars/" + image + ".jpg' alt='" + title.text() + "' />");
         title.before(imageElement);
+    }
+});
+
+$("ul.characters li").each(function() {
+    var char = $(this);
+    var text = char.text();
+    var subText = $("small", char).text();
+
+    text = text.replace(subText, "").trim();
+
+    var link = characterLinks[text];
+    if (link) {
+        var externalLink = $("<a target='_blank' href='" + link + "'><i class='fa fa-external-link'></i></a>");
+        char.append(externalLink);
     }
 });
